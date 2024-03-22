74e67ede47213b075b4dabb709fa0ead09ccb81f
ehsan <ehsanmadadialengareqhi@gmail.com>
2024-03-22 23:53:21 +0330

    add more formRezerv

--- components/user/pages/rezervTable/index.tsx
+++ components/user/pages/rezervTable/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import FormRezerv from './FormRezerv/index';
+import Menu from '@/components/user/pages/home/landing/menu/index';
+import Link from 'next/link';
+import { IoArrowBackCircleSharp } from "react-icons/io5";
+import Footer from '@/components/user/pages/home/footer/index';
 
 export default function RezervTable() {
   return (
-    <div>RezervTable</div>
+    <div className="bg-white" >
+      <div dir='ltr' className="p-5 m-2">
+        <Link className='text-blue-500 text-6xl md:text-3xl bg-white hover:text-blue-800' href='/resturant'> <IoArrowBackCircleSharp /> </Link>
+      </div>
+
+      <div className="bg-white dark:bg-slate-900 mb-5">
+        <FormRezerv />
+      </div>
+      <Footer />
+    </div>
+
+
+
   )
 }
