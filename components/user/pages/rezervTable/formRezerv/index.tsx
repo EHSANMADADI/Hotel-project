ea71654a408a32956b3e52b44b49e1e31c08f9db
ehsan <ehsanmadadialengareqhi@gmail.com>
2024-03-23 14:27:54 +0330

    add more to formRezervTable

--- components/user/pages/rezervTable/formRezerv/index.tsx
+++ components/user/pages/rezervTable/formRezerv/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 export default function FormRezerv() {
      return (
           <form className="max-w-2xl mx-auto my-10 p-5 bg-white dark:bg-slate-900">
@@ -30,10 +32,15 @@ export default function FormRezerv() {
                          <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                          <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">شماره تلفن</label>
                     </div>
-                    {/* <div className="relative z-0 w-full mb-5 group">
-       <input type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-       <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
-   </div> */}
+                    <div className="relative z-0 w-full mb-5 group">
+                         <input type="date" name="floating_date" id="floating_date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" تاریخ رزرو خود را انتخاب کنید" required />
+                         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">تاریخ رزرو </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                         <input type="time" name="floating_time" id="floating_time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" ساعت رزرو خود را انتخاب کنید" required />
+                         <label htmlFor="floating_time" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ساعت رزرو </label>
+                    </div>
+
                </div>
                <button type="submit" className="animate-bounce text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ثبت</button>
           </form>
--- components/user/pages/rezervTable/index.tsx
+++ components/user/pages/rezervTable/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FormRezerv from './FormRezerv/index';
+import FormRezerv from '@/components/user/pages/rezervTable/formRezerv/index';
 import Menu from '@/components/user/pages/home/landing/menu/index';
 import Link from 'next/link';
 import { IoArrowBackCircleSharp } from "react-icons/io5";
@@ -10,6 +10,15 @@ export default function RezervTable() {
     <div className="bg-white" >
       <div dir='ltr' className="p-5 m-2">
         <Link className='text-blue-500 text-6xl md:text-3xl bg-white hover:text-blue-800' href='/resturant'> <IoArrowBackCircleSharp /> </Link>
+      </div>
+      <div className='border rounded-lg p-5 m-5 leading-relaxed border-blue-400'>
+        <h6>شما میتوانید با تکمیل فرم زیر میز خود را در رستوران میلاد رزرو کنید</h6>
+        <p>لطفا برای اطلاعات بیشتر درباره ی قوانین موارد زیر را مطالعه فرمایید:</p>
+        <ul className='list-disc mr-8 mb-2 mt-2'>
+          <li>فقط در صورتی که مبلغ پیش پرداخت را واریز نمایید و کد پیگیری دریافت نمایید میز برای شما رزرو خواهد شد در غیر این صورت ما هیچ مسئولیتی نخواهیم داشت</li>
+          <li>میز از 15 دقیقه قبل از ساعت تعین شده توسط شما رزرو میشود و حد اکثر تا 30 دقیقه بعد از زمان تعیین شده توسط شما در رزرو باقی میماند</li>
+          <li>در صورتی که حداکثر تا 30 دقیقه بعد از زمان تعیین شده مراجعه نفرمایید رزرو شما باطل خواهد شد و امکان بازگرداندن مبلغ پیش پرداخت وجود نخواهد داشت</li>
+        </ul>
       </div>
 
       <div className="bg-white dark:bg-slate-900 mb-5">
