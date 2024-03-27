67cca168cbae628f0b1b69df4044c29c787fca73
ehsan <ehsanmadadialengareqhi@gmail.com>
2024-03-27 17:19:19 +0330

    add DatePicker persian

--- components/user/pages/rezervTable/formRezerv/index.tsx
+++ components/user/pages/rezervTable/formRezerv/index.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import Swal from 'sweetalert2';
 import { useRef } from 'react';
 import { Calendar } from 'react-multi-date-picker';
-import persian from'react-date-object/calendars/persian';
-import persian_fa from'react-date-object/locales/persian_fa';
+import DatePicker from "react-multi-date-picker";
+
+import persian from 'react-date-object/calendars/persian';
+import persian_fa from 'react-date-object/locales/persian_fa';
 
 export default function FormRezerv() {
      const FerstName = useRef(null);
@@ -14,7 +16,7 @@ export default function FormRezerv() {
      const Ocloack = useRef(null);
      const NumberGuset = useRef(null);
      return (
-          <form onSubmit={(e)=>e.preventDefault()} className="max-w-2xl mx-auto my-10 p-5 bg-white dark:bg-slate-900">
+          <form onSubmit={(e) => e.preventDefault()} className="max-w-2xl mx-auto my-10 p-5 bg-white dark:bg-slate-900">
                {/* <div className="relative z-0 w-full mb-5 group">
      <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
      <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
@@ -42,14 +44,39 @@ export default function FormRezerv() {
                          <input ref={PhoneNumber} type="tel" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                          <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">شماره تلفن</label>
                     </div>
-                    <div className="relative z-0 w-full mb-5 group">
-                         <input ref={DateRezerv} type="date" name="floating_date" id="floating_date" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" تاریخ رزرو خود را انتخاب کنید" required />
-                         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">تاریخ رزرو </label>
-                    </div>
                     {/* <div className="relative z-0 w-full mb-5 group">
-                         <Calendar calendar={persian} locale={persian_fa}/>
+                         <input ref={DateRezerv} type="date" name="floating_date" id="floating_date" 
+                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2
+                          border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" تاریخ رزرو خود را انتخاب کنید" required />
                          <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">تاریخ رزرو </label>
                     </div> */}
+                    <div className="relative z-0 w-full mb-5 group text-white">
+                        
+                      {/* <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">تاریخ رزرو </label> */}
+                         <DatePicker style={{
+                              display: 'block',
+                              padding:'20px 0px',
+                              width: '100%',
+                              fontSize: '0.8rem',
+                              lineHeight: '1.25rem',
+                              background:'transparent',
+                              borderWidth:'0px',
+                              borderBottomWidth:'2px',
+                              borderColor:'gray',
+                              appearance:'none',
+
+                         }} 
+                         containerStyle={{
+                              border:'none',
+                              width: "100%",
+                              outline:'none',
+                             
+                            }}
+                          calendar={persian} locale={persian_fa} placeholder='تاریخ رزرو خود را انتخاب نمایید'/>
+                    </div>
+                    
+                  
+                    
 
                     <div className="relative z-0 w-full mb-5 group">
                          <input ref={Ocloack} type="time" name="floating_time" id="floating_time" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" ساعت رزرو خود را انتخاب کنید" required />
