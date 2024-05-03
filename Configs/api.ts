2965ed24bf2c504526c18a953170eca5e2f4a6d5
ehsan <ehsanmadadialengareqhi@gmail.com>
2024-05-03 21:57:35 +0330

    fix

--- Configs/api.ts
+++ Configs/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from'axios'
 import { getCookie } from "@/utils/cookie";
 
 const api = axios.create({
--- app/loading.jsx
+++ app/loading.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Loading() {
      return (
-       <div className="flex justify-center text-center bg-white h-max-screen mt-72">
+       <div className="flex justify-center text-center bg-white dark:bg-slate-900 h-max-screen mt-72">
         <Image width={100} height={100}  src={"/loader/loader.gif"} alt="Loading.."/>
        </div>
      )
