b3e57665ec8a0feb858ff2a295c63e076a3a9cf6
Av808080 <108837984+Av808080@users.noreply.github.com>
2024-06-16 04:25:16 +0330

    Add table to coffeShop

--- components/user/pages/cafe/Admin/Table/AddTable.tsx
+++ components/user/pages/cafe/Admin/Table/AddTable.tsx
@@ -1,34 +1,78 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FormEvent } from 'react'
+import toast from 'react-hot-toast'
 import Title from '../Title'
+import api from '@/Configs/api'
 
 const AddTable = () => {
-    const tableNumberRef = useRef<HTMLInputElement>(null)
-    const tableCapacityRef = useRef<HTMLInputElement>(null)
-    const tableTypeRef = useRef<HTMLSelectElement>(null)
-    const submitHandler = (e: FormEvent) => {
+    const submitHandler = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-    } // Send Data To API And Validation
+        const formData = new FormData(e.currentTarget)
+        const number_of_chairs = formData.get("capacity");
+        const number_of_table = formData.get("tableNumber")
+        const rates = formData.get("price");
+        const type = formData.get("type");
+        const description = formData.get("description")
+
+        if (!number_of_chairs || Number.isNaN(+number_of_chairs) || +number_of_chairs <= 0) {
+            toast.error("ظرفیت میز را به درستی مشخص کنید.", { duration: 3000 })
+            return
+        }
+        if (!number_of_table) {
+            toast.error("شماره میز را مشخص کنید.", { duration: 3000 })
+            return
+        }
+        if (!rates || Number.isNaN(+rates) || +rates <= 0) {
+            toast.error("هزینه رزرو میز را به درستی مشخص کنید.", { duration: 3000 })
+            return
+        }
+
+        api.post("/coffee-shop/tables", {
+            coffee_shop_id: 1,
+            rates,
+            type,
+            number_of_chairs,
+            number_of_table,
+            description
+        }).then(() => {
+            toast.success("میز با موفقیت تعریف شد.")
+        }).catch(err => {
+            const statusCode = err?.response?.status;
+            if (statusCode === 422)
+                toast.error("تعریف میز با خطا مواجه شد. (اطلاعات ورودی کافی نمی باشد.)", { duration: 3000 })
+            else if (statusCode >= 500)
+                toast.error("تعریف میز با خطا مواجه شد. (خطای سمت سرور رخ داد.)", { duration: 3000 })
+            else
+                toast.error("تعریف میز با خطا مواجه شد.", { duration: 3000 })
+        })
+    }
     return (
         <>
             <Title>تعریف میز جدید</Title>
-            <form action="" onSubmit={submitHandler} className='flex flex-col items-center gap-4'>
+            <form onSubmit={submitHandler} className='flex flex-col items-center gap-4'>
                 <label htmlFor="Capacity" className='font-semibold'>ظرفیت میز : </label>
-                <input id='Capacity' placeholder='مثلا 2' ref={tableCapacityRef}
-                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2' 
-                    />
-
+                <input type='number' id='Capacity' name='capacity' placeholder='مثلا 2'
+                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2'
+                />
                 <label htmlFor="Type" className='font-semibold'>نوع میز : </label>
-                <select name="Type" id="Type" ref={tableTypeRef}
-                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2' 
-                    >
+                <select name="type" id="Type"
+                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2'
+                >
                     <option value="family">خانوادگی</option>
                     <option value="couple">عاشقانه</option>
                 </select>
 
                 <label htmlFor="TableNumber" className='font-semibold'>شماره میز : </label>
-                <input id='TableNumber' ref={tableNumberRef}
-                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2' 
+                <input id='TableNumber' name='tableNumber'
+                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2'
                     placeholder='مثلا 4' />
+                <label htmlFor="description" className='font-semibold'>توضیحات اختیاری: </label>
+                <textarea id='description' name='description'
+                    className='w-1/4 resize-none bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2'
+                    placeholder='توضیحات (اختیاری)' />
+                <label htmlFor="price" className='font-semibold'>قیمت میز برای رزرو (به تومان): </label>
+                <input id='price' name='price' type='number'
+                    className='w-1/4 bg-slate-50 dark:bg-zinc-900 ring-1 ring-coffee min-w-[200px] px-2 py-1 rounded-lg focus:outline-none focus:ring-2'
+                    placeholder='مثلا 120,000' />
                 <button className='bg-coffee hover:bg-coffee/90 duration-200 font-semibold px-6 py-1.5 rounded-lg text-slate-200'>ثبت میز</button>
             </form>
         </>
---
+++ components/user/pages/cafe/Admin/Table/TablesGrid.tsx
@@ -0,0 +1,61 @@
+import React, { ReactNode } from 'react'
+import { CiEdit } from "react-icons/ci";
+import { FaTrashAlt } from "react-icons/fa";
+import toast from 'react-hot-toast';
+import api from '@/Configs/api';
+import { useQueryClient } from '@tanstack/react-query'
+
+type TableType = {
+    tables: {
+        id: number,
+        type: "couple" | "family"
+        description?: string
+        rates: string
+        number_of_table: string
+        number_of_chairs: string
+    }[]
+}
+const TablesGrid = ({ tables }: TableType) => {
+    const queryClient = useQueryClient()
+    const deleteTable = (id: number) => {
+        try {
+            api.delete(`/coffee-shop/tables/${id}`)
+            toast.success("میز با موفقیت حذف شد.")
+            queryClient.invalidateQueries()
+        }
+        catch (error) {
+            toast.error("حذف میز با خطا مواجه شد")
+        }
+    }
+    return (
+        tables?.length === 0 ? <h2 className=' text-center font-bold text-xl py-8 animate-pulse'>در حال حاضر میز فعالی وجود ندارد </h2> :
+            <table className='rounded-xl px-6 py-10 dark:bg-slate-700 w-full text-center '>
+                <caption className='text-lg font-semibold mb-2'>لیست میزهای فعال</caption>
+                <th className='border border-indigo-400'>شماره میز</th>
+                <th className='border border-indigo-400'>ظرفیت میز</th>
+                <th className='border border-indigo-400'>نوع میز</th>
+                <th className='border border-indigo-400'>قیمت رزرو</th>
+                <th className='border border-indigo-400'>توضیحات</th>
+                <th className='border border-indigo-400' colSpan={0.3} >ویرایش و حذف</th>
+                {tables.map((table) => <tr key={table.id}>
+                    <TableData>{table.number_of_table}</TableData>
+                    <TableData>{table.number_of_chairs}</TableData>
+                    <TableData>{table.type === "couple" ? "عاشقانه" : "خانوادگی"}</TableData>
+                    <TableData>{Number(table.rates).toLocaleString()}</TableData>
+                    <TableData>{table.description || " - "}</TableData>
+                    <td className='border border-indigo-400 font-semibold flex justify-center items-center gap-6'>
+                        <CiEdit onClick={() => { }} className='cursor-pointer' color='#0ecb11' size={24} />
+                        <FaTrashAlt className='cursor-pointer' color='#e71212' size={20} onClick={() => deleteTable(table.id)} /></td>
+                </tr>)}
+            </table>
+
+    )
+}
+
+export default TablesGrid
+
+const TableData = ({ children }: { children: ReactNode }) => {
+    return <td className='border border-indigo-400 font-semibold'>
+        {children}
+    </td>
+}
