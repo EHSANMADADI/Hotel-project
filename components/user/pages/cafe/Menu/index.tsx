4ed57a924685985c8e25b3e7ca94c37480d8618e
Av808080 <108837984+Av808080@users.noreply.github.com>
2024-08-03 16:20:00 +0330

    Show Items Of Cafe Menu

--- components/user/pages/cafe/Menu/index.tsx
+++ components/user/pages/cafe/Menu/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import MenuItems from './menu-items'
+'use client'
+import React, { useEffect, useState } from 'react'
+// import MenuItems from './menu-items'
 import CardItem from './Card-item'
+import api from '@/Configs/api'
+import { MenuItems } from './types/MenuItems'
+
 const Menu = () => {
+    const [menuItems, setMenuItems] = useState([] as MenuItems[])
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const {data} = await api.get<MenuItems[]>('/menu-items')
+            const _menuItems = data.filter((item)=> item.type === 'coffeeShop')
+            setMenuItems(_menuItems)
+        }
+        fetchUsers()
+    })
     return (
         <section id='Menu' className='px-12 md:px-8 xl:px-16'>
-                <h2 className='text-white text-3xl font-bold text-center py-4'>منوی کافه</h2>
-                <div className='grid grid-cols-3 xxl:grid-cols-2 xl:grid-cols-1 lg:gap-x-8 gap-x-16 gap-y-8 px-6 lg:px-4 md:px-3'>
-                {MenuItems.map((item)=>{
+            <h2 className='text-white text-3xl font-bold text-center py-4'>منوی کافه</h2>
+            <div className='grid grid-cols-3 xxl:grid-cols-2 xl:grid-cols-1 lg:gap-x-8 gap-x-16 gap-y-8 px-6 lg:px-4 md:px-3'>
+                {menuItems.map((item) => {
                     return <CardItem item={item} key={item.id} />
                 })}
-                </div>
+            </div>
         </section>
     )
 }
---
+++ components/user/pages/cafe/Menu/types/MenuItems.ts
@@ -0,0 +1,9 @@
+export interface MenuItems {
+    id: number,
+    name: string
+    slug: string,
+    type: string,
+    description: string
+    price: string
+    media: string
+}
