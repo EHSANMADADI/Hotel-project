d78fe5791b434fe546d64b1c76b485ffdf0ed484
Ali khatami <akhatami1380t@gmail.com>
2024-08-30 18:46:50 +0330

    Show Modal

--- components/shared/modal/index.tsx
+++ components/shared/modal/index.tsx
@@ -3,22 +3,19 @@ import { IoIosCloseCircle } from "react-icons/io";
 
 export default function Modal(props: { Open: Boolean, onClose: () => void, children: React.ReactNode }) {
   if (!props.Open) return null;
-
-  const Handelclose = (e: any) => {
-    if (e.target.id === 'wrapper') props.onClose();
-
-  }
   return (
-    <div className='fixed inset-0 flex justify-center items-center transition-colors bg-opacity-25 z-50   border-black' id='wrapper' onClick={Handelclose}>
-      <div className='w-[800px] sm:w-[300px]  flex flex-col'>
+    <>
+    <div className='fixed inset-0 flex justify-center items-center transition-colors bg-opacity-25 z-50 border-black' >
+      <div className='top-0 left-0 bg-gray-700/50 absolute w-full h-screen -z-20'  onClick={props.onClose}/>
+      <div className='w-[800px] sm:w-[300px] flex flex-col'>
         <button className='text-black  place-self-end rounded p-2 mb-1' onClick={() => props.onClose()}><IoIosCloseCircle className='text-3xl bg-white' /></button>
-        <div className='bg-slate-500 dark:bg-slate-600  rounded '>
+        <div className='bg-slate-500 dark:bg-slate-600 rounded '>
           <div className='w-full'>
             {props.children}
           </div>
-
         </div>                                                
       </div>
     </div>
+    </>
   )
 }
--- components/user/pages/resturant/Product-selected/index.tsx
+++ components/user/pages/resturant/Product-selected/index.tsx
@@ -16,7 +16,7 @@ type ImageCardProps = {
 
 
 const Selected = ({ imageSrc, name, price, id, onClose , description}: ImageCardProps) => {
-    const [quantity, setQuantity] = useState();
+    const [quantity, setQuantity] = useState("");
 
     const cart = useCart()
     const dispatch = useSetCart()
@@ -25,7 +25,7 @@ const Selected = ({ imageSrc, name, price, id, onClose , description}: ImageCard
     const click = () => {
         quantity ?
             (
-                dispatch({ type: "ADD", payload: { id, price, name, quantity } }),
+                dispatch({ type: "ADD", payload: { id, price, name, quantity:+quantity } }),
                 console.log(cart),
                 Swal.fire({
                     title: "با موفقیت انجام شد",
@@ -38,19 +38,15 @@ const Selected = ({ imageSrc, name, price, id, onClose , description}: ImageCard
     return (
 
         <div className='w-full h-fit group relative flex overflow-hidden rounded-md  outline outline-2 outline-Theme-cream justify-between'>
-            <div>
                 <Image
                     src={imageSrc}
                     alt='img-food'
                     width={300}
                     height={500}
-                    className='w-full max-h-[400px] object-cover'
-                />
-            </div>
-
-
-            <div className='flex flex-col text-center m-2 p-1'>
-                <div><h2 className="text-2xl font-black">{name}</h2></div>
+                    className='w-3/5 max-h-[400px] object-cover'
+                    />
+            <div className='flex flex-col text-center m-2 p-1 flex-1'>
+                <><h2 className="text-2xl font-black">{name}</h2></>
                 <div className="font-black">{price}</div>
                 <div className="mt-5">
                     <p>{description}</p>
@@ -58,12 +54,12 @@ const Selected = ({ imageSrc, name, price, id, onClose , description}: ImageCard
                 <div className="my-5">
                     <form onSubmit={(e) => { e.preventDefault() }}>
                         <div className="relative  w-full mb-5 group text-white">
-                            <input value={quantity} onChange={(e) => { setQuantity(Number(e.target.value)) }} type="number" name="floating_number-Food" id="floating_number-Food" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300  dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 " required placeholder="تعداد" />
+                            <input value={quantity} onChange={(e) => { setQuantity(e.target.value) }} type="number" name="floating_number-Food" id="floating_number-Food" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300  dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 " required placeholder="تعداد" />
                             {/* <label htmlFor="floating_number-Guest" className="peer-focus:font-medium absolute text-sm text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> تعداد  </label> */}
                         </div>
                     </form>
                 </div>
-                <div className="my-5">
+                <div className="my-5 ">
                     <button onClick={click} className="transition-all p-2 bg-transparent border border-blue-200 rounded-md hover:bg-blue-300">افزودن به سبد خرید</button>
                 </div>
             </div>
