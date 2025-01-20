cc79d531672bb35d73c2074e67ad7a6e4132aad5
Ehsanmadadi <ehsanmadadialengareqhi@gmail.com>
2025-01-20 15:25:16 +0330

    change addres api

--- Configs/api.ts
+++ Configs/api.ts
@@ -2,7 +2,7 @@ import axios from'axios'
 import { getCookie } from "@/utils/cookie";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "http://87.107.54.23/api",
   headers: {
     Accept: "application/json",
   },
